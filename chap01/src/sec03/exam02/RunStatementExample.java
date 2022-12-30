6bb4ac655beb8e530ab629923104843f0e4efea8
seokjun982 <54785186+seokjun982@users.noreply.github.com>
2022-12-30 15:16:36 +0900

    add code

---
+++ chap01/src/sec03/exam02/RunStatementExample.java
@@ -0,0 +1,40 @@
+package sec03.exam02;
+
+public class RunStatementExample {
+	public static void main(String[] args) {
+		
+		
+		byte bytevalue = 10;
+		float floatvalue = 2.5F;
+		double doublevalue = 2.5;
+		
+		//byte result = bytevalue + bytevalue;
+		//int result = 5 + bytevalue;
+		//float result = 5 + floatvalue;
+		double result = 5 + doublevalue;
+		
+		
+		
+		
+		
+		
+		
+		
+		//int intvalue = 10;
+		//char charvalue = 'A';
+		//double doublevalue = 5.7;
+		//String strvalue ="A";
+		//byte bytevalue = 10;
+		//char charvalue = 'a';
+		//변수 x를 선언하고 1을 저장
+		//double var = (double)intvalue;
+		//byte var = (byte)intvalue;
+		//int var = (int)doublevalue;
+		//char var = (char)strvalue;
+		//int intvalue = charvalue;
+		//short shortvalue = charvalue;
+		//double doublevalue = bytevalue;
+		//변수 result를 선언하고 x와 y를 더한 값을 저장
+		System.out.println(var);		//모니터에 출력하는 메소드 호출
+	}
+}
